c139bc6c7b7d78e71e9743024ca9f8f79efaf369
Yuri Miranda Fialho <mirandafialho@gmail.com>
2024-01-18 20:50:43 -0300

    Change paths

--- apoiare/urls.py
+++ apoiare/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, re_path 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^blog/', include('blog.urls'))
 ]
